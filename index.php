39e0184a696fa1e30a72c050dbc3c2d0a30de685
AbdielP <abdiel.pinzonc@gmail.com>
2020-11-05 14:04:30 -0500

    Inicialización de proyecto 1 soft 7. index.php y modulo mantenimiento

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Encuestas aleatorias.</title>
+</head>
+<body>
+    <h2>Php es la peor porquería de lenguaje jamás creado que sigue siendo utilizado por la gente.</h2>
+    <a href="mantenimiento/mantenimiento.php">Administrar.</a>
+</body>
+</html>
