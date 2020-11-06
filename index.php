fa57505eaa1b0a5694a745d317f88ba11524bd26
ricardo0917 <ricmol83920970@gmail.com>
2020-11-05 20:03:47 -0500

    Cambio en  H2 index.html

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <title>Encuestas aleatorias.</title>
 </head>
 <body>
-    <h2>Php es la peor porquería de lenguaje jamás creado que sigue siendo utilizado por la gente.</h2>
+    <h2>Prueba de encuestas.</h2>
+
     <a href="mantenimiento/mantenimiento.php">Administrar.</a>
 
     <!-- BOOTSTRAP 4.5  -->
