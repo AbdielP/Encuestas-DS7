323c60b986af33d5114deb73303c5172adaccafe
AbdielP <abdiel.pinzonc@gmail.com>
2020-11-06 04:16:11 -0500

    Se termina componente de administración. se añaden estilos a sitio de administración

--- class/encuesta.php
+++ class/encuesta.php
@@ -7,12 +7,23 @@ class encuesta extends modeloCredencialesBD {
     }
 
     public function insertar_encuesta_binaria($form) {
-        // echo "<script type='text/javascript'>alert('$form');</script>";
+        $message = "Pregunta binaria insertada con éxito.";
         $instruccion = "INSERT INTO binarias (pregunta) VALUES ('".$form."')";
         $query = $this->__db->query($instruccion);
-        return "<p>Pregunta binaria insertada con éxito.</p>";
+        return '<script type="text/javascript">alert("' . $message . '")</script>';
         $query->close();
         $this->__db->close();
+    }
+
+    public function insertar_encuesta_multiple($pregunta, $tipo, $op1, $op2, $op3, $op4) {
+        $message = "Pregunta con opciones multiples insertada con éxito.";
+        $instruccion = "INSERT INTO multiples (pregunta, op1, op2, op3, op4, tipo) VALUES ('".$pregunta."','".$op1."',
+            '".$op2."','".$op3."','".$op4."','".$tipo."')";
+        $query = $this->__db->query($instruccion);
+        return '<script type="text/javascript">alert("' . $message . '")</script>';
+        $query->close();
+        $this->__db->close();
+        # multiples = 1, simple =0;
     }
 }
 ?>
--- mantenimiento/mantenimiento.php
+++ mantenimiento/mantenimiento.php
@@ -4,43 +4,124 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="../estilos/estilos.css">
     <title>Administración de preguntas</title>
 </head>
 <body>
-    <div class="container mt-3">
+    <div class="container mt-5 mb-5 p-5">
         <!-- TITULO  -->
         <div class="row">
-            <h2>Módulo de administración y mantenimiento de preguntas.</h2>
+            <h2>Módulo de administración y mantenimiento de preguntas <i class="fas fa-toolbox"></i></h2>
         </div>
         <hr>
-        <!-- FORM  -->
+        <!-- FORM BINARIO-->
         <div class="row">
             <div class="col-md-12">
-            <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Crear pregunta de elección binaria: (Si / No)</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" name="binaria" rows="3"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+                <h4>Creación de preguntas binarias <i class="fas fa-check-circle"></i></h4>
+                <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Crear pregunta de elección binaria: (Si / No)</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" name="binaria" rows="2" placeholder="Cree preguntas con respuestas binarias (si o no) con un máximo de 255 caracteres." required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Crear pregunta <i class="far fa-paper-plane"></i></button>
+                </form>
+            </div>
         </div>
+        <hr>
 
-    </div>
+        <!-- FORM SIMPLE Y MULTIPLE  -->
+        <div class="row">
+            <div class="col-md-12">
+                <h4>Creación de preguntas de selección multiple <i class="fas fa-list"></i></h4>
+                <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Crear pregunta de elección simple o elección múltiple.</label> <br>
+                        <small>Seleccione el tipo de pregunta, <b>simple</b> o <b>múltiple</b> en el campo 'tipo de pregunta'.</small>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" name="multiple" rows="2" placeholder="Ingrese aquí su pregunta. máximo 255 caracteres." required></textarea>
+                    </div>
+                    <!-- TIPO DE PREGUNTA  -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-check-inline">
+                                <h5>Tipo de pregunta:</h5>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="tipo" id="inlineRadio1" value="0" required>
+                                <label class="form-check-label" for="inlineRadio1"><i class="fas fa-list"></i> Simple</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="tipo" id="inlineRadio2" value="1" required>
+                                <label class="form-check-label" for="inlineRadio2"><i class="fas fa-tasks"></i> Multiple</label>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <!-- OPCIONES  -->
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="opcion1">Opción 1.</label> <br>
+                            <input id="opcion1" class="form-control form-control-sm" type="text" name="op1" placeholder="Opción/respuesta #1">
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="opcion2">Opción 2.</label> <br>
+                            <input id="opcion2" class="form-control form-control-sm" type="text" name="op2" placeholder="Opción/respuesta #2">
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="opcion3">Opción 3.</label> <br>
+                            <input id="opcion3" class="form-control form-control-sm" type="text" name="op3" placeholder="Opción/respuesta #3">
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="opcion4">Opción 4.</label> <br>
+                            <input id="opcion4" class="form-control form-control-sm" type="text" name="op4" placeholder="Opción/respuesta #4">
+                        </div>
+                        <div class="col-md-12">
+                            <small>Si la pregunta simple o multiple tiene menos de 4 opciones, deje vacíos los campos que no requiera.</small>
+                        </div>
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Crear pregunta <i class="far fa-paper-plane"></i></button>
+                </form>
+            </div>
+        </div>
     
-    <br><br>
-    <a href="../index.php">Volver</a>
+        <br><hr>
+        <a href="../index.php"><i class="far fa-arrow-alt-circle-left"></i> Volver al sitio de encuesta</a>
     </div>
 
     <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $name = $_POST['binaria'];
-            if (empty($name)) {
-                echo "Name is empty";
-            } else {
-                require_once("../class/encuesta.php");
-                $obj_crear_encuesta = new encuesta();
-                $resultado_encuesta = $obj_crear_encuesta->insertar_encuesta_binaria($name);
-                echo $resultado_encuesta;
+            // BINARIA 
+            if(isset($_REQUEST['binaria'])) {
+                $name = $_POST['binaria'];
+                if (empty($name)) {
+                    echo "Pregunta binaria vacía.";
+                } else {
+                    require_once("../class/encuesta.php");
+                    $obj_crear_encuesta = new encuesta();
+                    $resultado_encuesta = $obj_crear_encuesta->insertar_encuesta_binaria($name);
+                    echo $resultado_encuesta;
+                    return;
+                }
+            }
+            // MULTIPLES
+            if(isset($_REQUEST['multiple'])) {
+                $multiple = $_POST['multiple'];
+                $tipo = $_POST['tipo'];
+                $op1 = $_POST['op1'];
+                $op2 = $_POST['op2'];
+                $op3 = $_POST['op3'];
+                $op4 = $_POST['op4'];
+                if (empty($multiple)) {
+                    echo "Pregunta multiple vacía";
+                } else {
+                    require_once("../class/encuesta.php");
+                    $obj_crear_encuesta_multiple = new encuesta();
+                    $resultado_encuesta_multiple = $obj_crear_encuesta_multiple->insertar_encuesta_multiple($multiple,
+                        $tipo,$op1,$op2,$op3,$op4);
+                    echo $resultado_encuesta_multiple;
+                    return;
+                }
             }
         }
     ?>
