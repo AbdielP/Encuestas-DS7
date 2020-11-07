42e9030f64879e5a21c6d2893d5fb3ed696306b8
AbdielP <abdiel.pinzonc@gmail.com>
2020-11-06 20:56:54 -0500

    Estilos terminados, pendiente: post-envio de encuesta

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="estilos/estilos.css">
     <link rel="stylesheet" href="estilos/encuesta.css">
     <title>Encuestas aleatorias.</title>
 </head>
-<body class="bg-light">
-    <div class="container mt-5 mb-5">
+<body>
+    <div class="container mt-5 mb-5 p-5">
         <div class="col-md-12">
-            <h1>Encuesta DS-7</h1>
+            <h1>Encuesta DS-7 <i class="fas fa-feather-alt"></i></h1>
             <p class="lead">Gracias por participar en esta breve encuesta. Recuerde rellenar los datos del participante antes de proceder a contestar el formulario. La encuesta está compuesta por 10 preguntas de tipo binario y selección simple o múltiple.</p>
 
             <h3>Datos del participante.</h3>
@@ -22,7 +24,7 @@
 
                 <div class="row">
                     <div class="col-md-6 mb-3">
-                        <label for="state">Sexo</label>
+                        <label for="state">Sexo <i class="fas fa-male"></i> <i class="fas fa-female"></i></label>
                         <select class="custom-select d-block w-100" id="state" required>
                             <option value="" disabled selected>Seleccione su sexo...</option>
                             <option>Másculino</option>
@@ -31,7 +33,7 @@
                     </div>
 
                     <div class="col-md-6 mb-3">
-                        <label for="state">Rango de edad</label>
+                        <label for="state">Rango de edad <i class="far fa-calendar-alt"></i></label>
                         <select class="custom-select d-block w-100" id="state" required>
                             <option value="" disabled selected>Seleccione rango de edad...</option>
                             <option>Menos de 18 años. (-18)</option>
@@ -45,7 +47,7 @@
 
                 <div class="row">
                     <div class="col-md-6 mb-3">
-                        <label for="state">Rango salarial</label>
+                        <label for="state">Rango salarial <i class="far fa-money-bill-alt"></i></label>
                         <select class="custom-select d-block w-100" id="state" required>
                             <option value="" disabled selected>Seleccione su nivel de pobreza...</option>
                             <option>Colaborador de CWP</option>
@@ -57,7 +59,7 @@
                     </div>
 
                     <div class="col-md-6 mb-3">
-                        <label for="state">Provincia (PA)</label>
+                        <label for="state">Provincia (PA) <i class="fas fa-globe-americas"></i></label>
                         <select class="custom-select d-block w-100" id="state" required>
                             <option value="" disabled selected>Seleccione en que provincia reside.</option>
                             <option>Bocas Del Toro</option>
@@ -77,35 +79,37 @@
                 <hr class="mb-4">
                 <div class="row mb-2">
                     <div class="col-md-12">
-                        <h3>Cuestionario</h3>
+                        <h3>Cuestionario <i class="fas fa-check-double"></i></h3>
                     </div>
                 </div>
                 <?php
                     require_once("class/encuesta.php");
                     $obj_binarias = new encuesta();
+                    // BINARIA 
                     $encuestas_binarias = $obj_binarias->get_encuesta_binaria();
                     foreach ($encuestas_binarias as $encuesta_binaria) {
                         echo "<div class='row mb-4'>";
                         echo    "<div class='col-md-12 preguntas p-3'>";
-                        echo        "<h5>".$encuesta_binaria['pregunta']."</h5>";
+                        echo        "<h5><i class='far fa-flag'></i> ".$encuesta_binaria['pregunta']."</h5>";
                         echo        "<div class='form-check form-check-inline'>";
                         echo            "<input class='form-check-input' type='radio' name='tipo' id='inlineRadio1' value='1' required>";
-                        echo            "<label class='form-check-label' for='inlineRadio1'><i class='fas fa-list'></i> Si</label>";
+                        echo            "<label class='form-check-label' for='inlineRadio1'>Si</label>";
                         echo        "</div>";
                         echo        "<div class='form-check form-check-inline'>";
                         echo            "<input class='form-check-input' type='radio' name='tipo' id='inlineRadio2' value='0' required>";
-                        echo            "<label class='form-check-label' for='inlineRadio2'><i class='fas fa-tasks'></i> No</label>";
+                        echo            "<label class='form-check-label' for='inlineRadio2'>No</label>";
                         echo        "</div>";
                         echo    "</div>";
                         echo "</div>";
                     }
 
+                    // ELECCIÓN SIMPLE
                     $encuestas_multiples = $obj_binarias->get_encuesta_multiple();
                     foreach ($encuestas_multiples as $encuesta_multiple) {
                         if ($encuesta_multiple["tipo"] == 0) {
                             echo "<div class='row mb-4'>";
                             echo    "<div class='col-md-12 preguntas p-3'>";
-                            echo        "<h5>".$encuesta_multiple['pregunta']."</h5>";
+                            echo        "<h5><i class='far fa-flag'></i> ".$encuesta_multiple['pregunta']."</h5>";
                             if( $encuesta_multiple['op1'] != "") {
                                 echo         "<div class='form-check'>";
                                 echo            "<input class='form-check-input' type='radio' name='exampleRadios' id='exampleRadios1' value='option1'>";
@@ -141,11 +145,11 @@
                             echo    "</div>";
                             echo "</div>";
                         }
-
+                        // ELECCIÓN MULTIPLE
                         if ($encuesta_multiple["tipo"] == 1) {
                             echo "<div class='row mb-4'>";
                             echo    "<div class='col-md-12 preguntas p-3'>";
-                            echo        "<h5>".$encuesta_multiple['pregunta']."</h5>";
+                            echo        "<h5><i class='far fa-flag'></i> ".$encuesta_multiple['pregunta']."</h5>";
                             if( $encuesta_multiple['op1'] != "") {
                                 echo         "<div class='form-check'>";
                                 echo            "<input class='form-check-input' type='checkbox' name='examplecheckboxs' id='examplecheckboxs1' value='option1'>";
@@ -183,60 +187,8 @@
                         }
                     }
                 ?>
-                <!-- PREGUNTA BINARIA -->
-                <!-- <div class="row mb-4">
-                    <div class="col-md-12 preguntas p-3">
-                        <h5>Pregunta #1</h5>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="tipo" id="inlineRadio1" value="1" required>
-                            <label class="form-check-label" for="inlineRadio1"><i class="fas fa-list"></i> Si</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="tipo" id="inlineRadio2" value="0" required>
-                            <label class="form-check-label" for="inlineRadio2"><i class="fas fa-tasks"></i> No</label>
-                        </div>
-                    </div>
-                </div> -->
-
-                <!-- PREGUNTA SIMPLE -->
-                <!-- <div class="row mb-4">
-                    <div class="col-md-12 preguntas p-3">
-                        <h5>Pregunta #2</h5>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                                Default radio
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                            <label class="form-check-label" for="exampleRadios2">
-                                Second default radio
-                            </label>
-                        </div>
-                    </div>
-                </div> -->
-
-                <!-- PREGUNTA MULTIPLE  -->
-                 <!-- <div class="row mb-4">
-                    <div class="col-md-12 preguntas p-3">
-                        <h5>Pregunta #3</h5>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="examplecheckboxs" id="examplecheckboxs1" value="option1" checked>
-                            <label class="form-check-label" for="examplecheckboxs1">
-                                Default checkbox
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="examplecheckboxs" id="examplecheckboxs2" value="option2">
-                            <label class="form-check-label" for="examplecheckboxs2">
-                                Second default checkbox
-                            </label>
-                        </div>
-                    </div>
-                </div> -->
                 <hr class="mb-4">
-                <button class="btn btn-primary btn-lg btn-block" type="submit">Enviar encuesta</button>
+                <button class="btn btn-primary btn-lg btn-block" type="submit">Enviar encuesta <i class="far fa-paper-plane"></i></button>
             </form>
         </div>
     </div>
